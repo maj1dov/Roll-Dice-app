d06177ea31f4d6fb88d0457b62c1d960714c44c8
Yusif Majidov <yusif.majidov@aist.group>
2023-07-12 16:59:19 +0400

    fix:

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: GradientContainer(
-          color: const [
+          color: [
             Color.fromRGBO(0, 128, 128, 1),
             Color.fromRGBO(255, 0, 0, 1),
           ],
